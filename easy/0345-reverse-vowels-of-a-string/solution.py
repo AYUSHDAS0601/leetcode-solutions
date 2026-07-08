93f0615eef65ea722de1329baf2fa10e42a93b60
Ayush Das <156392534+AYUSHDAS0601@users.noreply.github.com>
2026-07-08 22:42:58 +0530

    ✅ Reverse Vowels of a String | Easy | Python | Runtime: 0 ms | Memory: 19.4 MB | 2026-07-08

--- easy/0345-reverse-vowels-of-a-string/solution.py
+++ easy/0345-reverse-vowels-of-a-string/solution.py
@@ -1,7 +1,6 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        vowels = {'a','e','i','o','u'}
-        s.lower()
+        vowels = {'a','e','i','o','u', 'A', 'E', 
         l, r = 0 , len(s)-1
         while l>r:
             if s[l] not in vowels:
@@ -12,4 +11,5 @@ class Solution:
                 s[l], s[r] = s[r], s[l]
                 right += 1
                 left += 1
+        'I', 'O', 'U'}
         return ''.join(s)
